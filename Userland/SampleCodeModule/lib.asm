bc6d81b179f0c2be0748ad47f02f5f36a77fa3f3
mbartellini <mbartellinihuapalla@gmail.com>
2021-06-01 17:37:32 -0300

    Test division by zero exception

--- Userland/SampleCodeModule/lib.asm
+++ Userland/SampleCodeModule/lib.asm
@@ -1,6 +1,24 @@
 GLOBAL print
+GLOBAL setReg
 
 print:
     mov rax, 1
     int 80h
+    ret
+
+setReg:
+    mov rax, 0xFFFF
+    mov rbx, 0xFFF0
+    mov rcx, 0xFF00
+    mov rdx, 0xF000
+    mov r8,  0xFF00
+    mov r9,  0xFFF0
+    mov r10, 0xFFFF
+    mov r11, 0xFFF0
+    mov r12, 0xFF00
+    mov r13, 0xF000
+    mov r14, 0xFF00
+    mov r15, 0xFFF0
+    div cl
+    mov ax, cx
     ret
--- Userland/SampleCodeModule/lib.h
+++ Userland/SampleCodeModule/lib.h
@@ -2,5 +2,6 @@
 #define __LIB_H__
 
 void print(uint8_t fd, char * s, uint64_t count);
+void setReg(void);
 
 #endif
