21fe071ac505974dc50c21480bdd139e97fc38be
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2021-06-04 19:37:12 -0300

    Add debug option in run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [["$1" = "gdb"]]; then
+if [[ "$1" = "gdb" ]]; then
     qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 -d int
 else
     qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 
