c60f681755997a6f75197348c4a33f7e6adeca8a
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2021-06-08 10:07:55 -0300

    Resolve merge conflict

--- run.sh
+++ run.sh
@@ -2,5 +2,9 @@
 if [[ "$1" = "gdb" ]]; then
     qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 -d int
 else
+<<<<<<< Updated upstream
     qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 
+=======
+    qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512
+>>>>>>> Stashed changes
 fi
