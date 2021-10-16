e37421dadb9651a62135ca8c6cec0c0d5d819125
BraveJero <jbrave271@gmail.com>
2021-10-16 17:42:39 -0300

    run clang-formater on userland files.

--- Userland/SampleCodeModule/include/cpuidflags.h
+++ Userland/SampleCodeModule/include/cpuidflags.h
@@ -2,16 +2,16 @@
 #define __CPUIDFLAGS_H__
 
 enum {
-    CPUID_FEAT_ECX_SSE3         = 1 << 0, 
-    CPUID_FEAT_ECX_PCLMUL       = 1 << 1,
-    CPUID_FEAT_ECX_SSE4_1       = 1 << 19, 
-    CPUID_FEAT_ECX_SSE4_2       = 1 << 20, 
-    CPUID_FEAT_ECX_AES          = 1 << 25,  
-    CPUID_FEAT_ECX_AVX          = 1 << 28,
-    
-    CPUID_FEAT_EDX_MMX          = 1 << 23, 
-    CPUID_FEAT_EDX_SSE          = 1 << 25, 
-    CPUID_FEAT_EDX_SSE2         = 1 << 26
+  CPUID_FEAT_ECX_SSE3 = 1 << 0,
+  CPUID_FEAT_ECX_PCLMUL = 1 << 1,
+  CPUID_FEAT_ECX_SSE4_1 = 1 << 19,
+  CPUID_FEAT_ECX_SSE4_2 = 1 << 20,
+  CPUID_FEAT_ECX_AES = 1 << 25,
+  CPUID_FEAT_ECX_AVX = 1 << 28,
+
+  CPUID_FEAT_EDX_MMX = 1 << 23,
+  CPUID_FEAT_EDX_SSE = 1 << 25,
+  CPUID_FEAT_EDX_SSE2 = 1 << 26
 };
 
 int32_t supports_cpuid(void);
