76f1a46e4bb33e1f621184cfff5bc7431b496b1d
juan <juanigarcia@itba.edu.ar>
2021-10-20 15:59:51 -0300

    Develop how we save each process.

--- Kernel/process.c
+++ Kernel/process.c
@@ -1,8 +1,29 @@
 #include <process.h>
+#include <mmgr.h>
 
+#define MAX_PROCESS_COUNT 256
+#define ALIVE 1
+#define TERMINATED 0
 
-uint64_t initFirstProcess(void) {
+typedef struct process {
+    uint8_t pid, status, priority;
+    uint64_t rsp;
+} Process;
 
+static Process * processes[MAX_PROCESS_COUNT];
+static uint8_t processCounter = 0;
 
+int saveProcess(uint64_t rsp, uint8_t priority) {
+    Process *newProcess = alloc(sizeof(Process));
+    if(newProcess == NULL){
+        return -1;
+    }
 
+    newProcess->pid = processCounter;
+    newProcess->status = ALIVE;
+    newProcess->rsp = rsp;
+    newProcess->priority = priority;
+    processes[processCounter++] = newProcess;
+    return 1;
 }
+
