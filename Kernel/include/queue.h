34d89082d657e5d9d39fe193da0065f1cf810783
juan <juanigarcia@itba.edu.ar>
2021-10-21 17:30:57 -0300

    Change scheduler's internal list from a circular list to a queue.

---
+++ Kernel/include/queue.h
@@ -0,0 +1,18 @@
+#ifndef __QUEUE_H__
+#define __QUEUE_H__
+
+#include <stdint.h>
+#include <process.h>
+
+typedef struct QueueCDT * Queue;
+typedef pid_t elemType;
+
+Queue newQueue(void);
+
+int push(Queue queue, elemType elem);
+
+elemType pop(Queue queue);
+
+uint8_t queueSize(Queue queue);
+
+#endif
---
+++ Kernel/queue.c
@@ -0,0 +1,65 @@
+#include <queue.h>
+#include <mmgr.h>
+
+typedef struct node {
+    elemType elem;
+    struct node *next;
+} Node;
+
+typedef struct QueueCDT {
+    uint8_t size;
+    Node *first, *last;
+} QueueCDT;
+
+Queue newQueue(void) {
+    Queue ans = alloc(sizeof(QueueCDT));
+    if (ans == NULL) {
+        return ans;
+    }
+    ans->size = 0;
+    ans->first = ans->last = NULL;
+    return ans;
+}
+
+int push(Queue queue, elemType elem) {
+    if (queue == NULL) {
+        return -1;  // TODO: Define errors
+    }
+
+    Node *newNode = alloc(sizeof(Node));
+    if (newNode == NULL) {
+        return -1;
+    }
+    newNode->elem = elem;
+    newNode->next = NULL;
+    
+    if (queue->first == NULL) {
+        queue->first = queue->last = newNode;
+    } else {
+        queue->last->next = newNode;
+        queue->last = newNode;
+    }
+
+    return ++queue->size;
+}
+
+elemType pop(Queue queue) {
+    if (queue == NULL || queue->first == NULL) {
+        return -1;
+    }
+
+    elemType elem = queue->first->elem;
+
+    if (queue->first == queue->last) {
+        queue->last = NULL;
+    }
+
+    Node *aux = queue->first;
+    queue->first = queue->first->next;
+    free(aux);
+    return elem;
+}
+
+uint8_t queueSize(Queue queue){
+    return queue == NULL ? 0 : queue->size;
+}
