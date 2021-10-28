4b1cc0ac869355447ed54ffa80c85bc53cefb8b9
BraveJero <jbrave271@gmail.com>
2021-10-28 00:07:38 -0300

    add todo.

--- Kernel/lib.c
+++ Kernel/lib.c
@@ -46,7 +46,7 @@ void *memcpy(void *destination, const void *source, uint64_t length) {
 }
 
 void acquire(int *mutex) {
-	while (exchange(mutex, 1) != 0);
+	while (exchange(mutex, 1) != 0);  // TODO: Re-Check if this is done correctly.
   return;
 }
 
