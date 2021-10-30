70745e29192e904ecd0c16e343700d2e8cdb5d1c
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2021-10-30 03:15:13 -0300

    Continue working on semaphores

--- Kernel/include/semaphore.h
+++ Kernel/include/semaphore.h
@@ -1,12 +1,27 @@
 #ifndef __SEMAPHORE_H__
 #define __SEMAPHORE_H__
 
+#include <stdint.h>
+
+#define MAX_SEMAPHORES  32
+
+enum SEM_RET {
+    SEM_SUCCESS = 0,
+    SEM_EXISTS,
+    SEM_INVALID,
+    SEM_ENOMEM,
+};
+
+typedef char lock_t;
+typedef uint8_t semid_t;
+typdef uint64_t semvalue_t;
+
 // C
 
 
 // ASM
-void _acquire(char *lock);
-void _release(char *lock);
+void _acquire(lock_t *lock);
+void _release(lock_t *lock);
 
 
 #endif
--- Kernel/semaphore.c
+++ Kernel/semaphore.c
@@ -1,48 +1,108 @@
 #include <semaphore.h>
+#include <stddef.h>
+#include <memoryManager.h>
 
-#define 
+static lock_t semaphoresLock = 0;
+struct semaphoreCDT * semaphores[MAX_SEMAPHORES] = {NULL};
 
 typedef struct semaphoreCDT {
-     char lock;
-    uint8_t value;
-    queueADT waiting;
+  lock_t lock;
+  semvalue_t value;
+  // queueADT waiting;
 } semaphoreCDT;
 
-
-int8_t createSemaphore(uint8_t id) {
-    // TODO: lock para manipular la lista de semaphores?
-    // Chequear si está libre el id
-    // si no, error
-    // Si sí, creamos
+static char isValidSemaphoreID(semid_t sid) {
+  return sid >= 0 && sid < MAX_SEMAPHORES;
 }
 
-void sem_wait(uint8_t id) {
-  _acquire(&(s.lock)) //spinlock (xchg)
+static char isActiveSemaphoreID(semid_t sid) {
+  return isValidSemaphoreID(sid) && semaphores[sid] != NULL;
+}
 
-  if (s.value > 0){
-    s.value--;
-  }else{
-    _release(&(s.lock))
-    // TODO: Add to queue and go to sleep(Blocked?) sleep(s.chan)
-    // se despierta solo cuando alguien hace post
-    _acquire(&(s.lock))
-    s.value--;
+enum SEM_RET createSemaphore(semid_t sid, semvalue_t value) {
+  if (!isValidSemaphoreID(sid))
+    return SEM_INVALID;
+  
+  _acquire(&semaphoresLock);
+  
+  if (isActiveSemaphoreID(id)) {
+    _release(&semaphoresLock);
+    return SEM_EXISTS;
+  }
+  
+  semaphores[sid] = alloc(sizeof(semaphoreCDT));
+  if (semaphores[sid] == NULL) {
+    _release(&semaphoresLock);
+    return SEM_ENOMEM;
   }
 
-  _release(&(s.lock)) //spinlock
+  semaphores[sid]->lock = 0;
+  semaphores[sid]->value = value;
+  _release(&semaphoresLock);
+  return SEM_SUCCESS;
 }
 
-sem_post(s){
-  _acquire(&(s.lock))
+enum SEM_RET waitSemaphore(semid_t sid) {
+  if (!isValidSemaphoreID(sid))
+    return SEM_INVALID;
+  
+  _acquire(&semaphoresLock);
+  if (!isActiveSemaphoreID(sid)) {
+    _release(&semaphoresLock);
+    return SEM_INVALID;
+  }
+  _acquire(&(semaphores[sid]->lock));
+  _release(&semaphoresLock);
 
-  s.value++;
-  // TODO: pop first from queue and remove Blocked state: wakeup(s.chan)
+  if (semaphores[sid]->value <= 0){
+    _release(&(semaphores[sid]->lock))
+    // sleep(s.chan)
+    // TODO: Add to queue and go to sleep(Blocked?) 
+    // se despierta solo cuando alguien hace post
+    _acquire(&(semaphores[sid]->lock))
+  }
 
-  _release(&(s.lock))
+  semaphores[sid]->value--;
+  _release(&(semaphores[sid]->lock))
+  return SEM_SUCCESS;
 }
 
-void closeSemaphore() {
+enum SEM_RET postSemaphore(semid_t sid) {
+  if (!isValidSemaphoreID(sid))
+    return SEM_INVALID;
+  
+  _acquire(&semaphoresLock);
+  if (!isActiveSemaphoreID(sid)) {
+    _release(&semaphoresLock);
+    return SEM_INVALID;
+  }
+  _acquire(&(semaphores[sid]->lock));
+  _release(&semaphoresLock);
 
+  semaphores[sid]->value++;
+  // wakeup(s.chan)
+
+  _release(&(semaphores[sid]->lock));
+  return SEM_SUCCESS;
+}
+
+enum SEM_RET closeSemaphore(semid_t sid) {
+  if (!isValidSemaphoreID(sid))
+    return SEM_INVALID;
+  
+  _acquire(&semaphoresLock);
+  if (!isActiveSemaphoreID(sid)) {
+    _release(&semaphoresLock);
+    return SEM_INVALID;
+  }
+  _acquire(&(semaphores[sid]->lock));
+
+  // Only destroy if there are no processes waiting
+  // Consider listening queue
+  free(semaphores[sid]);
+
+  _release(&semaphoresLock);
+  return SEM_SUCCESS;
 }
 
 
